9525ff48e7e89321393618337d32e9cc26c8aff4
yamahagal <158484058+yamahagal@users.noreply.github.com>
2026-03-08 23:28:38 +0300

    Исправлена ошибка в Inventory.cs - используется FindObjectOfType для поиска SaveSystem

--- Assets/Infima Games/Low Poly Shooter Pack/Code/Character/Inventory.cs
+++ Assets/Infima Games/Low Poly Shooter Pack/Code/Character/Inventory.cs
@@ -34,7 +34,13 @@ namespace InfimaGames.LowPolyShooterPack
         #region METHODS
 
         public void Awake() {
-            var saveSystem = new SaveSystem();
+            // Ищем SaveSystem в сцене, так как он может быть в другом namespace
+            var saveSystem = FindObjectOfType<SaveSystem>();
+            if (saveSystem == null)
+            {
+                Debug.LogError("SaveSystem не найден в сцене!");
+                return;
+            }
             data = saveSystem.Load("C:\\Unity\\Low Poly Shooter Pack v4.3\\Saves");
         }
 
